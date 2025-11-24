b38f47aa0ce3afad9004b5e5089b95ead116b995
Arek Krakowiak <369arek12@protonmail.com>
2025-11-24 19:59:51 +0000

    start coding the header component

---
+++ src/features/Header/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+
+    return (
+        <header>
+            <h1>Reddit Light</h1>
+            <div>
+                <input type="text" placeholder="Search Reddit..." />
+                <button>Search</button>
+            </div>
+        </header>
+    );
+}
