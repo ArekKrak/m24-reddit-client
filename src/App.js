bc67d87f85a0b103c084d5f9277cd6d71fbe0993
Arek Krakowiak <369arek12@protonmail.com>
2025-11-24 20:31:10 +0000

    create SubredditList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './features/Header/Header';
+import SubredditList from './features/SubredditList/SubredditList';
 
 function App() {
+  subredditsArray = ['news', 'python', 'javascript', 'reactjs'];
+
   return (
     <div className="App">
       <Header />
+      <SubredditList subredditsArray={subredditsArray} />
     </div>
   );
 }
