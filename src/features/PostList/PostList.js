1758b37aa58332641f7e9d2535c39b7a1ba6b885
Arek Krakowiak <369arek12@protonmail.com>
2025-11-25 20:31:14 +0000

    code the post list component

--- src/features/PostList/PostList.js
+++ src/features/PostList/PostList.js
@@ -1,1 +1,13 @@
 // The PostList component gets an array of posts, loops over them.
+import './PostCard';
+
+export default function PostList({ posts }) {
+
+    return (
+        <section className="postlist-style">
+            {posts.map((post) => (
+                <PostCard className='postlist-card' key={post.id} post={post}/>
+            ))}
+        </section>
+    );
+}
