acddf13cf73ef09c8fe5047a24823548d5c63831
Arek Krakowiak <369arek12@protonmail.com>
2025-11-25 20:11:33 +0000

    add mock posts to the main component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,40 @@ import SubredditList from './features/SubredditList/SubredditList';
 
 function App() {
   const MOCK_SUBREDDITS = ['news', 'python', 'javascript', 'reactjs'];
+  const MOCK_POSTS = [
+    {
+      id: '1',
+      title: 'Breaking: The President of the Russian Federation, Vladimir Vladimirovich Putin backs US plan for ending Ukraine war as Trump gives Kyiv deadline to accept',
+      author: 'Rorey Bosotti',
+      subreddit: 'news',
+      score: 155,
+      noOfComments: 221
+    },
+    {
+      id: '2',
+      title: 'Python 3.15.0 alpha 2',
+      author: 'Steve Dower',
+      subreddit: 'python',
+      score: 32,
+      noOfComments: 7
+    },
+    {
+      id: '3',
+      title: 'Node.js 22.18 LTS',
+      author: 'John Doe',
+      subreddit: 'javascript',
+      score: 50,
+      noOfComments: 4
+    },
+    {
+      id: '4',
+      title: 'Records & Tuples for React',
+      author: 'Sébastien Lorber',
+      subreddit: 'reactjs',
+      score: 39,
+      noOfComments: 12
+    },
+  ];
 
   return (
     <div className="App">
