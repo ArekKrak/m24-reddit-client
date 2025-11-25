bd47e87bd425957d89a2b385267415aaefc510e0
Arek Krakowiak <369arek12@protonmail.com>
2025-11-25 21:09:41 +0000

    bug hunt

--- src/features/PostList/PostCard.js
+++ src/features/PostList/PostCard.js
@@ -1,1 +1,13 @@
 // The PostCard component gets a single post and displays it nicely.
+import './PostCard.css';
+
+export default function PostCard({ post }) {
+
+    return (
+        <article className="postcard-article">
+            <div>r/{post.subreddit} • Posted by u/{post.author}</div>
+            <h3 className="postcard-heading">{post.title}</h3>
+            <footer className="postcard-footer">{post.score} upvotes • {post.noOfComments} comments</footer>
+        </article>
+    );
+}
--- src/features/PostList/PostList.js
+++ src/features/PostList/PostList.js
@@ -1,12 +1,13 @@
 // The PostList component gets an array of posts, loops over them.
-import './PostCard';
+import './PostList.css';
+import PostCard from "./PostCard";
 
 export default function PostList({ posts }) {
 
     return (
         <section className="postlist-style">
             {posts.map((post) => (
-                <PostCard className='postlist-card' key={post.id} post={post}/>
+                <PostCard key={post.id} post={post}/>
             ))}
         </section>
     );
