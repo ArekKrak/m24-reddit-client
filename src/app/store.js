e092cdd0a737c4f570f995b20c3bf7338a6d1f2b
Arek Krakowiak <369arek12@protonmail.com>
2025-11-27 20:15:41 +0000

    modify reddit api and store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+/* Temporary reducer, it just returns whatever state it gets. */
+const dummyReducer = (state = {}) => state;
+
 export const store = configureStore({
-  reducer: {
-    
-  },
+  reducer: dummyReducer
 });
