03f497a01b8fe635e2444f0be5cf4274267e10ba
Arek Krakowiak <369arek12@protonmail.com>
2025-11-29 04:44:22 +0000

    code posts slice

--- src/features/PostList/postsSlice.js
+++ src/features/PostList/postsSlice.js
@@ -5,3 +5,22 @@ const initialState = {
     status: "idle", // "idle", "loading", "succeeded", "failed"
     error: null     // error message or null
 };
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        setPosts(state, action) {
+            state.items = action.payload;
+        },
+        setStatus(state, action) {
+            state.status = action.payload;
+        },
+        setError(state,action) {
+            state.error = action.payload;
+        }
+    }
+});
+
+export const { setPosts, setStatus, setError } = postsSlice.actions;
+export default postsSlice.reducer;
