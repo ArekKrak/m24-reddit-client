881637c1220f8cb3ec24918950588f6baa0943ff
Arek Krakowiak <369arek12@protonmail.com>
2025-11-29 06:03:16 +0000

    delete redundant line of code

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from "../features/PostList/postsSlice";
 
-const dummyReducer = (state = {}) => state;
-
 export const store = configureStore({
   reducer: {
     posts: postsReducer
