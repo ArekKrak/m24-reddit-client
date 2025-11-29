7eab5f0d66aa9de3e405b062cd4516f65d3c5a6c
Arek Krakowiak <369arek12@protonmail.com>
2025-11-29 04:50:20 +0000

    rm comment

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from "../features/PostList/postsSlice";
 
-/* Temporary reducer, it just returns whatever state it gets. */
 const dummyReducer = (state = {}) => state;
 
 export const store = configureStore({
