e6aecc9a2179615f74a805d8beb3c0bddf49892e
Arek Krakowiak <369arek12@protonmail.com>
2025-11-29 04:47:27 +0000

    wire postsSlice to store slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from "../features/PostList/postsSlice";
 
 /* Temporary reducer, it just returns whatever state it gets. */
 const dummyReducer = (state = {}) => state;
 
 export const store = configureStore({
-  reducer: dummyReducer
+  reducer: {
+    posts: postsReducer
+  }
 });
