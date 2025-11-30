aedda0aab4eef7459d92c80c651112dda17ec3b7
Arek Krakowiak <369arek12@protonmail.com>
2025-11-30 02:30:22 +0000

    change indentation settings

--- src/features/PostList/PostCard.js
+++ src/features/PostList/PostCard.js
@@ -3,11 +3,11 @@ import './PostCard.css';
 
 export default function PostCard({ post }) {
 
-    return (
-        <article className="postcard-article">
-            <div>r/{post.subreddit} • Posted by u/{post.author}</div>
-            <h3 className="postcard-heading">{post.title}</h3>
-            <footer className="postcard-footer">{post.score} upvotes • {post.noOfComments} comments</footer>
-        </article>
-    );
+	return (
+		<article className="postcard-article">
+			<div>r/{post.subreddit} • Posted by u/{post.author}</div>
+			<h3 className="postcard-heading">{post.title}</h3>
+			<footer className="postcard-footer">{post.score} upvotes • {post.noOfComments} comments</footer>
+		</article>
+	);
 }
