62ce15aa2a5e820193714d5d63e1b5ba1aaf58b5
Arek Krakowiak <369arek12@protonmail.com>
2025-11-30 03:33:40 +0000

    add props to the post card component

--- src/features/PostList/PostCard.js
+++ src/features/PostList/PostCard.js
@@ -1,10 +1,10 @@
 // The PostCard component gets a single post and displays it nicely.
 import './PostCard.css';
 
-export default function PostCard({ post }) {
+export default function PostCard({ post, onSelectPost }) {
 
 	return (
-		<article className="postcard-article">
+		<article className="postcard-article" onClick={onSelectPost(post)}>
 			<div>r/{post.subreddit} • Posted by u/{post.author}</div>
 			<h3 className="postcard-heading">{post.title}</h3>
 			<footer className="postcard-footer">{post.score} upvotes • {post.noOfComments} comments</footer>
