fa678129166edefca55efa4e5113743d22fa837e
Arek Krakowiak <369arek12@protonmail.com>
2025-11-30 07:08:06 +0000

    import essential items to post detail modal add a constant to trigger the thunk add comments selectors and add effect

--- src/features/PostList/PostDetailModal.js
+++ src/features/PostList/PostDetailModal.js
@@ -1,4 +1,22 @@
+import React, { useEffect } from "react";	// `useEffect` to run the code when the modal opens
+import { useDispatch, useSelector } from "react-redux";		// `useDispatch` to dispatch the comments thunk and `useSelector` to read comments state from Redux
+import { fetchCommentsForPostThunk } from "./postsSlice";
+
 export default function PostDetailModal({ post, onClose }) {
+	/* Trigger the thunk */
+	const dispatch = useDispatch();
+
+	/* Read comments info from Redux */
+	const comments = useSelector((state) => state.posts.comments);
+	const commentsStatus = useSelector((state) => state.posts.commentsStatus);
+	const commentsError = useSelector((state) => state.posts.commentsError);
+
+	/* When the modal opens (or the post changes) fetch its comments */
+	useEffect(() => {
+		if (!post) return;		// safety: do nothing if post is missing
+		dispatch(fetchCommentsForPostThunk(post.id));
+	}, [dispatch, post]);
+
 	return (
 		<div className="modal-backdrop" onClick={onClose}>
 			<div className="modal-content" onClick={(e) => e.stopPropagation()}>
