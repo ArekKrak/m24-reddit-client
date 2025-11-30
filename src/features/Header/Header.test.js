500e78c1f528f0c54fc512d44dfa6bbf53db05da
Arek Krakowiak <369arek12@protonmail.com>
2025-11-30 00:51:27 +0000

    write test for the Header component

--- src/features/Header/Header.test.js
+++ src/features/Header/Header.test.js
@@ -2,3 +2,21 @@
     - has search call `onSearch` with trimmed text
     - has empty/whitespace input not call `onSearch` */
 
+import { render, screen, fireEvent } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Header", () => {
+    
+    it("renders the `onSearch` calls with trimmed text", () => {
+        const handleSearch = jest.fn();
+        const input = screen.getByPlaceholderText("Search Reddit...");
+        fireEvent.change(input, { target: { value: " python " } });
+        const button = screen.getByRole("button", { name: /search/i });
+        fireEvent.click(button);
+
+        expect(handleSearch).toHaveBeenCalledTimes(1);
+        expect(handleSearch).toHaveBeenCalledWith("python");
+
+        render(<Header onSearch={handleSearch}/>)
+    });
+});
